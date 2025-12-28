a8e202a7cd0fac227e8cf8cc189e9a8b3634171d
AbdoShebaika <abdalrahim.shebaika@gmail.com>
2025-12-29 01:24:52 +0200

    arrays

--- ship.js
+++ ship.js
@@ -4,6 +4,7 @@ let inventoryCount = 0
 let isEngineFunctional = true
 let destinationSector = "Alpha"
 
+
 let crateType = "Fuel"
 switch(crateType){
     case "Medical":
@@ -39,3 +40,44 @@ if(fuelLevel > 80 && isEngineFunctional){
 }else{
     console.log("CRITICAL HIT: Ship grounded.")
 }
+
+
+let crewList = ["Alice", "Bob", "Charlie", "Diana"]
+console.log(`the first name is ${crewList[0]} and the third name is ${crewList[2]}`)
+
+if(crewList.length>3){
+    console.log("Crew capacity reached.")
+}
+
+// 0000 as number?
+let entercode = 1234
+let isAuthorized = false
+switch(entercode){
+    case 1234:
+        isAuthorized = false
+        break
+    
+    case 0000:
+        console.log("Emergency override")
+        isAuthorized = true
+        break
+        
+    default:
+        isAuthorized = false
+        break    
+}
+
+if(isAuthorized){
+    console.log("Welcome, Captain.")
+}else{
+    console.log("ALARM: Intruder Alert!")
+}
+//let for suitTybe
+ let planetGravity = 1.9
+ if(planetGravity > 1.5){
+     suitType =  "Heavy Armor"
+ }else{
+     suitType = "Lightweight Suit"
+ }
+ console.log( `Equipping ${suitType} for landing.`)
+ 
